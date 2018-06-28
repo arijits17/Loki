f8d739a7c5147702f114f0d451567ebea858ea2c
arijits17 <arijitshaw95@gmail.com>
2018-06-28 19:31:38 +0530

    Add files via upload

--- friend.py
+++ friend.py
@@ -1,6 +1,19 @@
 import os
 import re
+import time
 from to_file import *
+
+"""
+This function takes a C code. and Daikon invariants generated on it. Puts those back at 
+place on main function as assertion.
+
+Intended to use as checking soundness of the Daikon invariants.
+
+TODO: works faulty if not well indented. 
+TODO: all invariants are not parsed actually.
+
+Uses some functions defined in to_file. 
+"""
 
 
 def put_invariants_as_assert(c_code_orig_file, target_file, daikon_result_file):
@@ -10,50 +23,92 @@ def put_invariants_as_assert(c_code_orig_file, target_file, daikon_result_file):
 	instrmented_lines = []
 	code_w_assert = list(c_code_lines)
 
-
 	line_it = 4
 	runcount = 0
+	insert_loc = 0
+
 
 
 	while line_it < len(daikon_text) :
 		line = daikon_text[line_it]
-		print "[DEBUG] " + str(line_it) + " " + daikon_text[line_it] 
 		if line.startswith(".."):
-			if line.startswith("..udon_dummy_"):		#take_care of other functions too	
-				func_name = line[2:]
-				func_name = func_name.rstrip("):::ENTER")
-				func_name = func_name.rstrip("):::EXIT")
-				print "[DEBUG] Current Function name : " + func_name 
+			
+			print "[DEBUG OUT] Current Function name : " + line
+			
+			if ("ENTER" in line):		
+				
+				func_name = line[2:].rstrip("):::ENTER")
 
-				while not line.startswith("===="):
-					assert_st = invariant_to_assert(line)
-					# print "[DEBUG] Why it loops here? A"
-					print code_w_assert
-					print assert_st
-					for inv_file_num, inv_file_line in enumerate(code_w_assert):
-						# print "[DEBUG] Why it loops here? B"
-						print code_w_assert
-
-						if((func_name in inv_file_line) and (");" in inv_file_line)) :
-							# print "[DEBUG] Why it loops here? C" 
-
-							code_w_assert.insert(inv_file_num+1, assert_st)
-							print "[ASSERT INSERTED] "+ assert_st
-							break
+				for inv_file_num, inv_file_line in enumerate(code_w_assert):
+					if((func_name in inv_file_line)  and (";}" not in inv_file_line)):
+						insert_loc = inv_file_num+2
+						# print "[DEBUG ] Insertion Loc : " + str(insert_loc)
+						print "[DEBUG] "+func_name + " starts at "+str(inv_file_num + 1)
 
 
-					if(line_it < len(daikon_text)) :
-						line_it += 1
+
+			else:
+				
+				func_name = line[2:].rstrip("):::EXIT")
+
+
+				it = 0
+
+				while not ((func_name in code_w_assert[it]) and (";}" not in code_w_assert[it])):
+					it += 1
+				
+				function_running = True
+				parenthesis_balance = 0
+
+				while function_running:
+					it += 1
+					if ((parenthesis_balance + code_w_assert[it].count('{') - code_w_assert[it].count('}'))>0):
+						print "[DEBUG ]parenthesis_balance  at line" + str(it+1) + "is" + str(parenthesis_balance) 
+						it += 1
 					else:
-						break
+						function_running = False  			#ASSURE CODE INDENTED WELL OR DO SOMETHING TO BREAK INTO }-s
+						print "[DEBUG] "+func_name + " ends at "+str(it+ 1)
 
-		if(line_it < len(daikon_text)) :
+				insert_loc = it -1
+				
+				# print  Insertion Loc : " + str(insert_loc) 
+
+
+
+
+
+
+				
 			line_it += 1
-		else:
-			break
+			line = daikon_text[line_it]
+			
+			# asserts_here = set([])
 
-	print_invariants_to_file(code_w_assert)
+			while not (line.startswith("====")):
+
+				assert_st = invariant_to_assert(line)
+
+
+				if (assert_st != ""):
+					print "[DEBUG IN] Current Function name : " + func_name 
+
+					code_w_assert.insert(insert_loc, assert_st)
+					print "[ASSERT INSERTED] "+ assert_st
+					break
+
+				line_it += 1
+				line = daikon_text[line_it]
+
+
+		line_it += 1
+
+	print_invariants_to_file(code_w_assert, target_file)
 
 
 
 put_invariants_as_assert("c_code.c","new_c_code.c","daikon_result")
+start_time = time.time()
+os.system("cbmc --all-properties new_c_code.c > cbmc_output.txt")
+print "CBMC took "+ str(time.time() - start_time) + " seconds."
+
+
--- to_file.py
+++ to_file.py
@@ -23,8 +23,8 @@ def print_to_file(lines_of_file, new_file_name):
 
 
 
-def print_invariants_to_file(lines_of_file):
-	code_with_sound_invarint = open("c_code_w_inv.c","w+")
+def print_invariants_to_file(lines_of_file, target_file):
+	code_with_sound_invarint = open(target_file,"w+")
 
 	for line in lines_of_file:
 		if "udon_dummy" not in line:
@@ -37,45 +37,119 @@ def print_invariants_to_file(lines_of_file):
 
 
 def invariant_to_assert(line):
-	if ("orig" not in line ):							
-		assert_st = "assert(" + line[2:] + ");"
+	if ("orig" in line ):							
+		return ""
+	if(line.startswith("::")):
+		line = line[2:]
+	if("return" in line) or ("argc" in line) or ("argv" in line) or ("null" in line):
+		return ""
+	if("::" in line):
+		line_braeak = line.split("::")
+		line = ''
+		for item in line_braeak: 
+			line += item
+	if("one of" in line):
+		return ""
+	if(re.search("[0-9]",line)) or (re.search("x",line)):		#eita gonjamil ache. chaper somosya
+		return ""
+	return "assert(" + line + ");"
+
+#"""
+
+def isDeclaration(line):
+	datatypes = ["int","char", "float", "long", "void"]
+	line_break = set(re.split(';|,| ',line)) - set([''])
+	type = list(set(datatypes).intersection(line_break))
+	if type != []:
+		return True
 	else:
-		assert_st = ""
+		return False
 
-	return assert_st
+def copy_variables(line):
+	datatypes = ["int","char", "float", "long", "void"]
 
-"""
+	if '(' not in line:
+		print "[DEBUG] Type 1 detected"
+		line_break = set(re.split(';|,| ',line)) - set([''])
+		# print line_break
+		type = list(set(datatypes).intersection(line_break))
+		# if(type != []):
+		# 	# print "[DEBUG] Type found "+ type[0]
+		variables = line_break - set(type)
+		opu_variables = type[0]+' '
+		for item in variables:
+			opu_item = "opu_"+item
+			opu_variables += opu_item+" = "+item+","
+		
+		opu_variables += "mjo;"
+		return [opu_variables]
+
+	else:
+		print "[DEBUG] Type 2 detected"
+
+		line_break = re.split(';|,|\(|\)| ',line)
+		# print line_break
+		it = -1
+		type_cnt = 0
+		while type_cnt<2:
+			it += 1
+			if (it == len(line_break) - 1):
+				return []
+			if line_break[it] in datatypes:
+				type_cnt += 1
+		opu_variables = []
+		while it < len(line_break):
+			if line_break[it] in datatypes:
+				opu_item_type = line_break[it]
+			elif line_break[it] == '' or line_break[it] == ' ':
+				pass
+			else:
+				opu_variables.append(opu_item_type+" opu_"+line_break[it]+" = "+line_break[it]+";")
+			it += 1	
+			 			
+		return opu_variables
+
+
+
+# print copy_variables("int x, y, z;")
+# print copy_variables("void f (float x, int y, int z);")
+
+
+
 
 def instrument_to_retain_orig_variable(orig_filename, new_filename):
 	c_code_lines = [c_code.rstrip('\n') for c_code in open(orig_filename)]
 	instrmented_code = open(new_filename, "w+")
 
-	instrmented_lines = []
 
-	datatypes = ["int","char", "float", "long"]
+	it = 0
 
-	for line_num, line in enumerate(c_code_lines):
-		type = list(set(datatypes).intersection(re.split(';|,| ',line)))
-		if(type != []):
-			print "[DEBUG] Type found "+ type[0]
+	while it < len(c_code_lines):
+		line = c_code_lines[it]
+		print "[DEBUG] "+line
+		if(isDeclaration(line)):
+			print "[DEBUG] line given to instrument"
+
+			to_insert = copy_variables(line)
+			for item in to_insert:
+				it += 1
+				c_code_lines.insert(it, item)
+		it += 1		
 
 
 
 
 
-
-
-
-	for line in instrumented_lines:
+	for line in c_code_lines:
 		print "[CPRINT ] " + line
 		print>>instrmented_code, line
 
 	instrmented_code.close()
 
+instrument_to_retain_orig_variable("c_code.c", "opu_c_code.c")
 
 
-
-
+"""
 
 
 
